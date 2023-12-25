8b69d9d92b352e7adcc2adf97a951110acb61840
Ali Faisal <alifaisalaslam@gmail.com>
2023-12-25 20:52:53 +0500

    added integration test for GET API with jsonToObject logic

--- src/test/java/org/scalable/amigoscodetests/student/StudentIntegrationTest.java
+++ src/test/java/org/scalable/amigoscodetests/student/StudentIntegrationTest.java
@@ -1,7 +1,9 @@
 package org.scalable.amigoscodetests.student;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +12,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,6 +29,11 @@ public class StudentIntegrationTest {
     private StudentRepository studentRepository;
     @Autowired
     private MockMvc apiController;
+
+    @AfterEach
+    public void tearDown(){
+        studentRepository.deleteAll();
+    }
 
     @Test
     public void testStudentCreation() throws Exception {
@@ -49,14 +58,34 @@ public class StudentIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(Objects.requireNonNull(jsonStudent.get()));
 
+        var getRequest = get("http://localhost:8080/api/v1/students")
+                .contentType(MediaType.APPLICATION_JSON);
+
         // Act
         var postAction = apiController.perform(postRequest);
 
         // Assert
         postAction.andExpect(status().isOk());
 
+        // Assertion through GET API
+        var getStudentJson = apiController.perform(getRequest).andReturn().getResponse().getContentAsString();
+        Optional<List<Student>> students = jsonToObject(getStudentJson, new TypeReference<List<Student>>() {});
+        Assertions.assertTrue(students.isPresent());
+        Assertions.assertTrue(students.get().stream().anyMatch(s -> s.getEmail().equals(student.getEmail())));
+
+        // Assertion through Repository
         var isStudentCreated = studentRepository.selectExistsEmail(student.getEmail());
         Assertions.assertTrue(isStudentCreated);
+    }
+
+    private <T> Optional<T> jsonToObject(String json, TypeReference<T> type){
+        try{
+            return Optional.of(new ObjectMapper().readValue(json, type));
+        }
+        catch (JsonProcessingException ex){
+            fail("Failed to convert json to object.");
+            return Optional.empty();
+        }
     }
 
     private Optional<String> objectToJson(Object object){

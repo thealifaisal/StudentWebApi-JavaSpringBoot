621d58cc4bc6e5fb92774e3a7d9a6943c6d3efb0
Ali Faisal <alifaisalas99@gmail.com>
2023-12-26 15:27:09 +0500

    wrapping any type of exception thrown by jsonToObject() into JsonToObjectMappingException

--- src/main/java/org/scalable/student_registration_web_api/utilities/mapper/JsonObjectMapper.java
+++ src/main/java/org/scalable/student_registration_web_api/utilities/mapper/JsonObjectMapper.java
@@ -1,17 +1,14 @@
 package org.scalable.student_registration_web_api.utilities.mapper;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.util.Optional;
 
 public class JsonObjectMapper {
     public static <T> T jsonToObject(String json, TypeReference<T> type) throws JsonToObjectMappingException {
         try{
             return new ObjectMapper().readValue(json, type);
         }
-        catch (JsonProcessingException ex){
+        catch (Exception ex){
             throw new JsonToObjectMappingException(ex);
         }
     }
